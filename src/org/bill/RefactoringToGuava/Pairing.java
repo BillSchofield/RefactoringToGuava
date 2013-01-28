47dfd200be52e390263b4dcf7488a9b377d805fd
Bill Schofield <bill@billschofield.net>
2013-01-28 09:02:03 -0600

    Flipped switch and loops to decouple compare methods

--- src/org/bill/RefactoringToGuava/Pairing.java
+++ src/org/bill/RefactoringToGuava/Pairing.java
@@ -12,41 +12,59 @@ public class Pairing {
     private static final int COMPARE_FLIGHT_DUTY = 0;
     private static final int COMPARE_WORK_BLOCK = 1;
 
-    public Pairing findLeastRestBetweenPairings(Line line, int ruleRest, int compareMethod) {  //MB 9/2012 Remove brief & debrief.  Previously set to 0.
-        Pairing aPair = null;
-        List<Pairing> pairingArray = createPairingRuleArray(line);
-        List<Pair<Pairing, Pairing>> pairingPairs = pairUpNeighbors(pairingArray);
-        List<LinePairing> linePairingArray = createLinePairingRuleArray(line);
-        List<Pair<LinePairing, LinePairing>> linePairingPairs = pairUpNeighbors(linePairingArray);
+    public Pairing findLeastRestBetweenPairings(Line line, int ruleRest, int compareMethod) {
+        List<Pair<Pairing, Pairing>> pairingPairs = pairingPairs(line);
+        Iterator<Pair<LinePairing, LinePairing>> linePairingPairIterator = linePairingPairIterator(line);
+        switch (compareMethod) {
+            case COMPARE_FLIGHT_DUTY:
+                return compareFlightDuty(ruleRest, pairingPairs, linePairingPairIterator, isDebugOn(line));
+            case COMPARE_WORK_BLOCK:
+                return compareWorkBlock(ruleRest, pairingPairs, linePairingPairIterator);
+            default:
+                return null;
+        }
+    }
+
+    private Pairing compareWorkBlock(int ruleRest, List<Pair<Pairing, Pairing>> pairingPairs, Iterator<Pair<LinePairing, LinePairing>> linePairingPairIterator) {
         Integer leastRest = null;
-        Iterator<Pair<LinePairing,LinePairing>> linePairingPairIterator = linePairingPairs.iterator();
-        boolean debugOn = isDebugOn(line);
         for (Pair<Pairing, Pairing> pairingPair : pairingPairs) {
             Pair<LinePairing, LinePairing> linePairingPair = linePairingPairIterator.next();
-            switch (compareMethod) {
-                case COMPARE_FLIGHT_DUTY:
-                    aPair = getLinePairingRestTimeByDutyBetweenPairings(pairingPair, linePairingPair, ruleRest, debugOn);
-                    if (aPair != null) {
-                        return aPair;
-                    }
-                    break;
-                case COMPARE_WORK_BLOCK:
-                    Integer rest = getLinePairingRestTimeByWorkBlock(pairingPair, linePairingPair);
-                    if ((leastRest == null) && (rest != 0) && !ignoreDaylightSavingsEffect(rest)) {
-                        leastRest = rest;
-                    }
-                    if ((leastRest != null) && (rest < leastRest.intValue()) && (rest != 0) && !ignoreDaylightSavingsEffect(rest)) {
-                        leastRest = rest;
-                    }
-                    if (leastRest != null) {
-                        if (leastRest.intValue() < ruleRest) {
-                            return pairingPair.fst;
-                        }
-                    }
-                    break;
+            Integer rest = getLinePairingRestTimeByWorkBlock(pairingPair, linePairingPair);
+            if ((leastRest == null) && (rest != 0) && !ignoreDaylightSavingsEffect(rest)) {
+                leastRest = rest;
+            }
+            if ((leastRest != null) && (rest < leastRest.intValue()) && (rest != 0) && !ignoreDaylightSavingsEffect(rest)) {
+                leastRest = rest;
+            }
+            if (leastRest != null) {
+                if (leastRest.intValue() < ruleRest) {
+                    return pairingPair.fst;
+                }
             }
         }
-        return aPair;
+        return null;
+    }
+
+    private Pairing compareFlightDuty(int ruleRest, List<Pair<Pairing, Pairing>> pairingPairs, Iterator<Pair<LinePairing, LinePairing>> linePairingPairIterator, Boolean isDebugOn) {
+        for (Pair<Pairing, Pairing> pairingPair : pairingPairs) {
+            Pair<LinePairing, LinePairing> linePairingPair = linePairingPairIterator.next();
+            Pairing pair = getLinePairingRestTimeByDutyBetweenPairings(pairingPair, linePairingPair, ruleRest, isDebugOn);
+            if (pair != null) {
+                return pair;
+            }
+        }
+        return null;
+    }
+
+    private List<Pair<Pairing, Pairing>> pairingPairs(Line line) {
+        List<Pairing> pairingArray = createPairingRuleArray(line);
+        return pairUpNeighbors(pairingArray);
+    }
+
+    private Iterator<Pair<LinePairing, LinePairing>> linePairingPairIterator(Line line) {
+        List<LinePairing> linePairingArray = createLinePairingRuleArray(line);
+        List<Pair<LinePairing, LinePairing>> linePairingPairs = pairUpNeighbors(linePairingArray);
+        return linePairingPairs.iterator();
     }
 
     private Integer getLinePairingRestTimeByWorkBlock(Pair<Pairing, Pairing> pairingPair, Pair<LinePairing, LinePairing> linePairingPair) {
